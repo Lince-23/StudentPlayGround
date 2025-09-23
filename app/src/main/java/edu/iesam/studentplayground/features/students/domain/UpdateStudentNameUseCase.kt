73ebc5f8aeab48f2a3b9f1a13b207064b539486f
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-09-23 20:03:41 +0200

    Se añade lógica al entorno de UpdateStudentNameUseCase.kt

--- app/src/main/java/edu/iesam/studentplayground/features/students/domain/UpdateStudentNameUseCase.kt
+++ app/src/main/java/edu/iesam/studentplayground/features/students/domain/UpdateStudentNameUseCase.kt
@@ -1,11 +1,15 @@
 package edu.iesam.studentplayground.features.students.domain
 
+import android.util.Log
+
 class UpdateStudentNameUseCase(val studentRepository: StudentRepository) {
     operator fun invoke(exp:String, changeStudent: Student){
         val existingStudent: Student? = studentRepository.getStudent(exp)
 
         if(existingStudent != null) {
             studentRepository.update(exp, changeStudent)
+        }else{
+            Log.d("@dev", "El usuario que se intenta actualizar no existe")
         }
     }
 }
